78abe00d94222953363efa80764e18ab2e6213b4
Edson <erodriguez43@ucol.mx>
2021-08-21 12:22:11 -0500

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), //123181272938
+            date: new Date().toDateString(), // sat 21, agust (o algo así)
+            text:'Labore magna culpa cupidatat ex mollit eu deserunt laboris. Ad et culpa commodo excepteur aliquip sint sint eu sit. In quis exercitation ipsum do duis velit tempor consectetur qui veniam proident. Elit nostrud aute ad sit eu proident exercitation qui dolore elit ut veniam magna velit. Velit mollit aliqua deserunt reprehenderit irure enim irure dolore mollit. Ipsum elit laborum mollit non non commodo quis laborum duis dolor aliquip consectetur minim eiusmod.',
+            picture: null, //https://kaslhdsak
+        },
+        {
+            id: new Date().getTime() + 1000, //123181272938
+            date: new Date().toDateString(), // sat 21, agust (o algo así)
+            text:'Nostrud elit occaecat laborum anim culpa cillum Lorem laboris laboris irure consectetur laborum. Ullamco nisi reprehenderit laborum ad ea. Laboris non eiusmod ad mollit Lorem. Anim nostrud adipisicing quis minim proident sunt ea exercitation irure est. Proident ad ad non aliqua occaecat dolore qui adipisicing.',
+            picture: null, //https://kaslhdsak
+        },
+        {
+            id: new Date().getTime() + 2000, //123181272938
+            date: new Date().toDateString(), // sat 21, agust (o algo así)
+            text:'Anim dolore et dolor nostrud Lorem enim eiusmod Lorem sint excepteur ullamco ipsum eu non. Ad officia laborum mollit voluptate laborum anim enim ipsum mollit ut. Adipisicing do dolore eiusmod ut sint irure deserunt nisi ex nostrud irure nisi amet. Amet et veniam sint dolor elit eiusmod tempor deserunt ut. Eu adipisicing tempor deserunt ipsum consectetur ea id adipisicing eiusmod pariatur elit laboris. In laboris eiusmod exercitation nostrud ut consectetur deserunt eu.',
+            picture: null, //https://kaslhdsak
+        },
+    ]
+})
